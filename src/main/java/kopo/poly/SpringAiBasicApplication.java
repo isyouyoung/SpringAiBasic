4bb01e02dde933981f9ad0bc11f2c1069677bcc6
isyouyoung <isyouyoung@gmail.com>
2025-06-12 20:26:12 +0900

    커밋

--- src/main/java/kopo/poly/SpringAiBasicApplication.java
+++ src/main/java/kopo/poly/SpringAiBasicApplication.java
@@ -1,19 +1,22 @@
 package kopo.poly;
 
-import kopo.poly.dto.OcrDTO;
-import kopo.poly.service.IOcrService;
+import kopo.poly.dto.StudentDTO;
+import kopo.poly.service.impl.StudentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @SpringBootApplication
 public class SpringAiBasicApplication implements CommandLineRunner {
 
-    private final IOcrService ocrService;
+
+    private final StudentService studentService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAiBasicApplication.class, args);
@@ -24,21 +27,60 @@ public class SpringAiBasicApplication implements CommandLineRunner {
 
         log.info("자바 프로그래밍 시작!!");
 
-        String filePath = "image";
-        String fileName = "sample01.jpg";
+//        String filePath = "image";
+//        String fileName = "sample01.jpg";
+//
+//        OcrDTO pDTO = new OcrDTO();
+//
+//        pDTO.setFilePath(filePath);
+//        pDTO.setFileName(fileName);
+//
+//        OcrDTO rDTO = ocrService.getReadforImageText(pDTO);
+//
+//        String result = rDTO.getResult();
+//
+//        log.info("인식된 문자열");
+//        log.info(result);
+//
+//        log.info("자바 프로그래밍 종료!!");
 
-        OcrDTO pDTO = new OcrDTO();
+        StudentDTO pDTO;
+        List<StudentDTO> rList;
 
-        pDTO.setFilePath(filePath);
-        pDTO.setFileName(fileName);
+        // 학생 등록하기
+        pDTO = new StudentDTO();
 
-        OcrDTO rDTO = ocrService.getReadforImageText(pDTO);
+        pDTO.setUserId("hglee67");
+        pDTO.setUserName("이협건");
+        pDTO.setEmail("hglee67@kopo.ac.kr");
+        pDTO.setAddr("서울");
 
-        String result = rDTO.getResult();
+        rList = studentService.insertStudent(pDTO);
 
-        log.info("인식된 문자열");
-        log.info(result);
+        rList.forEach(dto ->{
+            log.info("DB에 저장된 아이디 : " + dto.getUserId());
+            log.info("DB에 저장된 이름 : " + dto.getUserName());
+            log.info("DB에 저장된 이메일 : " + dto.getEmail());
+            log.info("DB에 저장된 주소 : " + dto.getAddr());
+        });
 
-        log.info("자바 프로그래밍 종료!!");
+        pDTO = new StudentDTO();
+
+        pDTO.setUserId("hglee67");
+        pDTO.setUserName("이협건_수정");
+        pDTO.setEmail("hglee67@kopo.ac.kr_수정");
+        pDTO.setAddr("서울_수정");
+
+        rList = studentService.updateStudent(pDTO);
+
+        rList.forEach(dto ->{
+            log.info("DB에 저장된 아이디 : " + dto.getUserId());
+            log.info("DB에 저장된 이름 : " + dto.getUserName());
+            log.info("DB에 저장된 이메일 : " + dto.getEmail());
+            log.info("DB에 저장된 주소 : " + dto.getAddr());
+        });
+
+
+
     }
 }
---
+++ src/main/java/kopo/poly/service/IStudentService.java
@@ -0,0 +1,13 @@
+package kopo.poly.service;
+
+import kopo.poly.dto.StudentDTO;
+
+import java.util.List;
+
+public interface IStudentService {
+
+    List<StudentDTO> insertStudent(StudentDTO pDTO) throws Exception;
+
+    List<StudentDTO> updateStudent(StudentDTO pDTO) throws Exception;
+
+}
